b5a8cd6cbeccea4af646de1271873c481a64462f
Francis Coulilbaly <Francis Coulilbaly@172.29.94.194>
2017-09-18 11:18:06 -0400

    first

---
+++ src/bonjournom/BonjourNom.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bonjournom;
+import aleatoire.Person; // sert à utiliser une librairie.
+
+/**
+ *
+ * @author Francis Coulilbaly
+ */
+public class BonjourNom {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour Ibrahim Francis Junior Coulibaly.");
+        Person zied = new Person("zied", "Zaier", 33);
+        System.out.println(zied.getFirstName());
+        System.out.println(zied.getLastName());
+        System.out.println(zied.getAge());
+        zied.setFirstName("Jean");
+        System.out.println(zied.getFirstName());
+    }
+    
+}
